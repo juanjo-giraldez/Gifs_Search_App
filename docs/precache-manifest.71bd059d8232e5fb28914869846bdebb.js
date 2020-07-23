43a41e58ccabcd33622d5092fcdf697960a1c72e
juanjo giraldez <juanjose.giraldez@kairosds.com>
2020-07-23 19:03:03 +0200

    Folder docs added

---
+++ docs/precache-manifest.71bd059d8232e5fb28914869846bdebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0432b1292850b5ec88a8404e01aa56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5a945ec49fde53b8f71",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "45a8a9424300d70a54f1",
+    "url": "/static/css/main.89eab715.chunk.css"
+  },
+  {
+    "revision": "b5a945ec49fde53b8f71",
+    "url": "/static/js/2.a458cc51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a458cc51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a8a9424300d70a54f1",
+    "url": "/static/js/main.b7996a7e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
